a44af26dcf39272114e400c765e64c899fdcbf49
Sebastian Schmittner <Echsecutor@users.noreply.github.com>
2020-04-11 23:28:49 +0200

    typo corrected

--- star.js
+++ star.js
@@ -37,7 +37,7 @@ function Star(bh_img) {
       image(this.img, this.object.position.x - this.scale_img*this.object.size.x/2, this.object.position.y - this.scale_img*this.object.size.y/2, this.scale_img*this.object.size.x, this.scale_img*this.object.size.y);
       fill(255, 255, 255);
     }
-    vat label = Math.floor(this.object.mass).toString();
+    var label = Math.floor(this.object.mass).toString();
     text(label, this.object.position.x - 5*label.length, this.object.position.y + 6);
   }
 
